d8dd8e96a5eaf572c4f4f648100059b97eb8dd29
Glibn <romanuting@gmail.com>
2025-04-23 15:26:26 +0300

    d

--- gog.js
+++ gog.js
@@ -1,2 +1,7 @@
 const result = 5 + 5 + "5";
-console.log(result);
+// console.log(result);
+// const email = "run@run.run";
+// const checkEmail = email.includes("@");
+// console.log(checkEmail);
+// console.log(email.length);
+console.log(f);
