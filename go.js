755447ff60b89229f32f6cb254c241826d7e90e7
Glibn <romanuting@gmail.com>
2025-04-25 13:25:14 +0300

    Update go.js

--- go.js
+++ go.js
@@ -4,4 +4,23 @@ const email = "run@run.run";
 const checkEmail = email.includes("@");
 console.log(checkEmail);
 console.log(email.length);
-console.log("f");
+console.log(email);
+const myName1 = "My";
+const myName2 = "name";
+const myName3 = "is";
+const fullName =
+  myName1 +
+  " " +
+  myName2 +
+  " " +
+  myName3 +
+  " " +
+  "victor";
+console.log(fullName);
+const userName = "Oleg";
+const payment = "200$";
+console.log(
+  alert(
+    `Дякуємо за покупку ${userName} кешбек ${payment}`
+  )
+);
